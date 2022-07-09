e5261098f8bfc4d04173deca9a18e564824aa69d
amit1366 <69159563+amit1366@users.noreply.github.com>
2022-07-09 22:20:27 +0530

    update

---
+++ src/component/Formtext.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react'
+
+export default function Formtext(props) {
+
+    // function for set text to uppercase
+    const handelupclick = () => {
+        let newText = text.toUpperCase();
+        setText(newText)
+    }
+
+    // function for on change text 
+    const handelonchange = (event) => {
+        // console.log(event.target.value)
+        setText(event.target.value)
+    }
+
+    const [text, setText] = useState('')
+    return (
+        <>
+            <div className="container  my-4">
+                <div>
+                    <h2>{props.Heading}</h2>
+                    <div className="mb-3">
+
+                        <textarea className="form-control" value={text} onChange={handelonchange} id="exampleFormControlTextarea1" rows="6"></textarea>
+                    </div>
+                    <button type="button " onClick={handelupclick} className="btn btn-primary mx-2">uppercase</button>
+
+                    <button type="button" className="btn btn-success mx-2">lovercase</button>
+                </div>
+            </div>
+            <div className="container  my-4">
+                <h1>Count no of word and carrector</h1>
+                <p>{text.split(" ").length} word and {text.length} character</p>
+                <p> {0.008 * text.split(" ").length} Mineats read</p>
+                <h1>Preview</h1>
+                <p>{text}</p>
+            </div>
+        </>
+    )
+}
